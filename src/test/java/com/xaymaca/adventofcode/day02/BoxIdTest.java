71a94ca223dcddbcdfe80d21f624a393e38eb7f7
Vincent Stoessel <vstoessel@central1.com>
2018-12-12 01:28:02 -0500

    fixed input file , completed half of day 2

--- src/test/java/com/xaymaca/adventofcode/day02/BoxIdTest.java
+++ src/test/java/com/xaymaca/adventofcode/day02/BoxIdTest.java
@@ -8,30 +8,40 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.*;
 
 public class BoxIdTest {
 
     @Test
     public void testChecksum() throws IOException {
 
-        InputStream inputs = loadInputs();
 
         int twoList = 0;
         int threeList = 0;
 
-        BufferedReader read = new BufferedReader(new InputStreamReader(inputs));
+        BufferedReader read = new BufferedReader(new InputStreamReader(loadInputs()));
         String line;
         while ((line = read.readLine()) != null) {
 
             if (hasXChars(2, line)) {
                 twoList++;
             }
-            if (hasXChars(3, line)) {
+
+        }
+        BufferedReader read2 = new BufferedReader(new InputStreamReader(loadInputs()));
+        String line2;
+        int lines = 0;
+        while ((line2 = read2.readLine()) != null) {
+
+            if (hasXChars(3, line2)) {
                 threeList++;
             }
+
+            lines++;
         }
-        int checksum = twoList * threeList ;
-        Assert.assertEquals("we found ", 85, checksum);
+        int checksum = twoList * threeList;
+        System.out.println("twos: " + twoList + " * threes: " + threeList);
+        Assert.assertEquals("we found ", 5904, checksum);
     }
 
 
@@ -47,8 +57,29 @@ public class BoxIdTest {
 
 
     private boolean hasXChars(int howManyToCheckFor, String line) {
-        //real work here
-        return false;
+        //real work goes here
+        boolean has = false;
+
+        char[] letters = line.toCharArray();
+
+        Map<Character, Integer> chars = new HashMap<>();
+        for (char currentLetter : letters) {
+            if (null == chars.get(currentLetter)) {
+                chars.put(currentLetter, 1);
+            } else {
+                int count = chars.get(currentLetter);
+                chars.put(currentLetter, count + 1);
+            }
+        }
+        // System.out.println(chars);
+
+        for (Integer value : chars.values()) {
+            // System.out.println("Value = " + value);
+            if (value == howManyToCheckFor) {
+                return true;
+            }
+        }
+        return has;
     }
 
 
